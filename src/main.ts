13ed4c68c3de2db77145a044d4c6f31f639386c6
Cu4lQu1er4 <correoquenuncauso123@gmail.com>
2026-03-11 15:16:43 -0500

    Añadimos crud para horarios

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,6 @@ async function bootstrap() {
       'http://localhost:3000',
       'https://work.nerpelsas.com'
     ],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   
